20770b7b5f17c76db4c56a27f095a599ad8627d6
Poggugglb <khachapuryan80@bk.ru>
2024-09-21 17:04:30 +0300

    add pagination

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'time-impala'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMIN = ['gobeklitele@gmail.com']
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # ADMIN = ['gobeklitele@gmail.com']
+    POSTS_PER_PAGE=5
